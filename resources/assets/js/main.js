8cc4691183191f2f626199cbc611748983b33cef
Gleb Starkov <glebstarkov@gmail.com>
2017-10-15 16:38:08 +0800

    Электронный счётчик ходов

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -9,8 +9,9 @@ function newGame() {
 }
 
 function fillLamps(elect) {
+    $('.elect-area .area').html('');
     for(var key in elect.lamps) {
-        $('.elect-area .row').append('<div class="lamp" id="lamp-' + elect.lamps[key].id + '"></div>');
+        $('.elect-area .area').append('<div class="lamp" id="lamp-' + elect.lamps[key].id + '"></div>');
     }
     $('.elect-area .lamp').click(function () {
         newMove(this);
@@ -27,6 +28,7 @@ function newMove(obj) {
     var id = $(obj).attr('id').replace(/^lamp\-(\d+)/, '$1');
     $.post('/move', {_token: _token, id: id}, function (data) {
         changeLamps(data.elect);
+        changeMoves(data.elect.move);
     });
 }
 
@@ -36,5 +38,15 @@ function changeLamps(elect) {
         if (elect.lamps[key].active) {
             $('#lamp-' + elect.lamps[key].id).addClass('active');
         }
+    }
+}
+
+function changeMoves(n) {
+    $('.digit').removeClass('digit-1').removeClass('digit-2').removeClass('digit-3').removeClass('digit-4').removeClass('digit-5').removeClass('digit-6').removeClass('digit-7').removeClass('digit-8').removeClass('digit-9');
+    var ns = n.toString();
+    var d = 0;
+    for(var i=ns.length-1; i>-1; i--) {
+        $('.d-' + d).addClass('digit-' + ns[i]);
+        d++;
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,15 @@
 @section('content')
     <h1>Electrician</h1>
     <div class="elect-area">
-        <div class="row">
+        <div class="row area">
+        </div>
+        <div class="row digits d-6">
+            <div class="digit d-5"></div>
+            <div class="digit d-4"></div>
+            <div class="digit d-3"></div>
+            <div class="digit d-2"></div>
+            <div class="digit d-1"></div>
+            <div class="digit d-0"></div>
         </div>
     </div>
 @endsection
